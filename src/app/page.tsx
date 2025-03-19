c80f89a8bcf4b7b6875a5efe7993e3e21735e15e
Dev-shannn <nishantpandeyy07@gmail.com>
2025-03-19 12:09:03 +0530

    worked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-import Hero from "./hero/hero";
-import Navbar from "./navbar/navbar";
-import FeaturedBlogs from './pages/topEngineering/featuredblog'
+  import Hero from "./hero/hero";
+  import Navbar from "./navbar/navbar";
+  import FeaturedBlogs from './pages/topEngineering/featuredblog'
 
-export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <main style={{ paddingTop: "1px" }}>
-        <Hero/>
-        {/* Additional page content */}
-        <FeaturedBlogs />
-      </main>
-    </div>
-  );
-}
+
+  export default function Home() {
+    return (
+      <div>
+        <Navbar />
+        <main style={{ paddingTop: "1px" }}>
+          <Hero/>
+          {/* Additional page content */}
+          <FeaturedBlogs />
+        </main>
+      </div>
+    );
+  }
